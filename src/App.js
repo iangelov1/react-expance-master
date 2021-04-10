ab40f4a8b3ce7a4401645336c89c3c7bef579e04
ivan-angelov <62991796+ivan-angelov@users.noreply.github.com>
2021-04-10 13:30:08 +0300

    initial expance commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Provider } from "react-redux";
+import "./App.css";
+
+import Nav from "./components/Nav";
+import Expence from "./components/Expence";
+import History from "./components/History";
+import Model from "./components/Model";
+import store from "./Store/Store";
+
+const App = () => {
+    return (
+        <Provider store={store}>
+            <div>
+                <Nav/>
+                <Expence />
+                <History />
+                <Model/>
+            </div>
+        </Provider>
+    );
+}
+
+export default App;
